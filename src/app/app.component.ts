f454f0ab69656ccd3d0b0e18c401085db5a49279
Jakub Białkowski <34075140+Lid3r@users.noreply.github.com>
2024-01-01 20:18:02 +0100

    Added a navbar with sidenav and logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+const MATERIALS = [
+  MatToolbarModule,
+  MatButtonModule,
+  MatIconModule,
+  MatSidenavModule,
+];
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MATERIALS],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'dziupla-app';
+
+  showFiller = false;
 }
