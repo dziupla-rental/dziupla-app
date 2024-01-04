9fe51cf00bc64125bed67de6cf9d753d4d2af6b1
Paweł Chmielewski <pawel.chmielewski@devtactics.net>
2024-01-04 15:01:35 +0100

    general stats

--- src/app/general-stats/general-stats.component.ts
+++ src/app/general-stats/general-stats.component.ts
@@ -1,13 +1,40 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {MatTableModule} from '@angular/material/table';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+
+export interface StatElement {
+  name: string;
+  icon: string;
+  value: number;
+}
+
+const ELEMENT_DATA: StatElement[] = [
+  {name: 'Liczba aut', icon: 'directions_car', value: 2137},
+  {name: 'Liczba aut wyporzyczonych', icon: 'sell', value: 2137},
+  {name: 'Liczba aut serwisie', icon: 'build', value: 2137},
+  {name: 'Liczba aut w lokalach', icon: 'home', value: 2137},
+  {name: 'Liczba klientów', icon: 'face', value: 2137},
+  {name: 'Liczba lokali', icon: 'store', value: 2137},
+  {name: 'Liczba pracowników', icon: 'accessibility', value: 2137},
+  
+];
 
 @Component({
   selector: 'app-general-stats',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, MatIconModule, MatCardModule],
   templateUrl: './general-stats.component.html',
   styleUrl: './general-stats.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class GeneralStatsComponent {
 
+
+export class GeneralStatsComponent {
+  displayedColumns: string[] = ['icon', 'name', 'value'];
+  dataSource = ELEMENT_DATA;
 }
+
+
+
+
--- src/app/owner-view/owner-view.component.ts
+++ src/app/owner-view/owner-view.component.ts
@@ -4,11 +4,12 @@ import {MatGridListModule} from '@angular/material/grid-list';
 import { PercentageIndicatorComponent } from '../percentage-indicator/percentage-indicator.component';
 import {MatCardModule} from '@angular/material/card';
 import { FinancialReportComponent } from '../financial-report/financial-report.component';
+import { GeneralStatsComponent } from '../general-stats/general-stats.component';
 
 @Component({
   selector: 'app-owner-view',
   standalone: true,
-  imports: [MatGridListModule, PercentageIndicatorComponent, MatCardModule, FinancialReportComponent],
+  imports: [MatGridListModule, PercentageIndicatorComponent, MatCardModule, FinancialReportComponent, GeneralStatsComponent],
   templateUrl: './owner-view.component.html',
   styleUrl: './owner-view.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
