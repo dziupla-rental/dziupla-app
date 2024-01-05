bf6c79f80c8d0c0e41ad16ee1bd3bf564579f5c3
Paweł Chmielewski <pawel.chmielewski@devtactics.net>
2024-01-05 16:15:29 +0100

    employeedDetails

--- src/app/components/management-view/employee-details/employee-details.component.ts
+++ src/app/components/management-view/employee-details/employee-details.component.ts
@@ -1,13 +1,26 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+
+export interface PersonData {
+    first_name: string;
+    last_name: string;
+    position: string;
+    id: number;
+    salary: number;
+    shift_start: number;
+    shift_end: number;
+    office: string;
+}
+
 
 @Component({
   selector: 'app-employee-details',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './employee-details.component.html',
   styleUrl: './employee-details.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class EmployeeDetailsComponent {
-
+  @Input() personData?: PersonData
 }
--- src/app/components/management-view/management-view.component.ts
+++ src/app/components/management-view/management-view.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeDetailsComponent, PersonData } from './employee-details/employee-details.component';
 
 export interface EmployeeListing {
   name: string;
@@ -16,26 +17,51 @@ export interface ManagementData {
 @Component({
   selector: 'app-management-view',
   standalone: true,
-  imports: [MatCardModule, FlexLayoutModule, EmployeeListComponent],
+  imports: [
+    MatCardModule,
+    FlexLayoutModule,
+    EmployeeListComponent,
+    EmployeeDetailsComponent,
+  ],
   templateUrl: './management-view.component.html',
   styleUrl: './management-view.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ManagementViewComponent {
+  personResponse? : PersonData= {
+    first_name: "Steve",
+    last_name: "Gomez",
+    position: "Mechanik",
+    id: 12,
+    salary: 12.3,
+    shift_start: 800,
+    shift_end: 2000,
+    office: "Gliwice",
+    };
   responseData?: ManagementData = {
-    "employees": [
+    employees: [
       { name: 'John Deere', id: 1 },
       { name: 'Jan Paweł 2', id: 2 },
       { name: 'Zbigniew Wodecki', id: 3 },
       { name: 'Adam Małysz', id: 4 },
-      ],
-    "offices": [
-      "brazil",
+      { name: 'John Deere', id: 1 },
+      { name: 'Jan Paweł 2', id: 2 },
+      { name: 'Zbigniew Wodecki', id: 3 },
+      { name: 'Adam Małysz', id: 4 },
+      { name: 'John Deere', id: 1 },
+      { name: 'Jan Paweł 2', id: 2 },
+      { name: 'Zbigniew Wodecki', id: 3 },
+      { name: 'Adam Małysz', id: 4 },
+      { name: 'John Deere', id: 1 },
+      { name: 'Jan Paweł 2', id: 2 },
+      { name: 'Zbigniew Wodecki', id: 3 },
+      { name: 'Adam Małysz', id: 4 },
+      { name: 'John Deere', id: 1 },
+      { name: 'Jan Paweł 2', id: 2 },
+      { name: 'Zbigniew Wodecki', id: 3 },
+      { name: 'Adam Małysz', id: 4 },
     ],
-    "positions": [
-      "your mom",
-    ],
+    offices: ['brazil'],
+    positions: ['your mom'],
   };
-
-
 }
